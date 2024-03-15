30b8b702c72ba09befd6e9ba553f42ea10946877
Lienid <0xLienid@protonmail.com>
2024-03-14 23:01:54 -0500

    Initial Commit

---
+++ focus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  focus
+//
+//  Created by Matt Firth on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ focus/focusApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusApp.swift
+//  focus
+//
+//  Created by Matt Firth on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct focusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
